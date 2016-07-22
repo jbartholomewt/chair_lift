59e14696fc98afc56fd9de80213dd7dcb2f0ff57
jbartholomewt <jbartholomewt@gmail.com>
2016-07-22 13:16:37 -0400

    storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :lifts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true
