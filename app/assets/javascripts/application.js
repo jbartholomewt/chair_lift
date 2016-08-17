b0ae16556af34256bb9641cdbbe0e3065993922b
jbartholomewt <jbartholomewt@gmail.com>
2016-08-17 12:18:09 -0400

    added geocoder for markers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,13 @@
 var marker;
 var map;
 var request;
+var lat = '';
+var lng = '';
+var geocoder;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40, lng: -70},
-    zoom: 6
+    zoom: 5
   });
 
  request = $.ajax({
@@ -31,11 +34,19 @@ function initMap() {
    });
    request.done(function(data) {
      data.lifts.forEach(function(lift){
+       geocoder = new google.maps.Geocoder();
+       geocoder.geocode( { 'address': lift.zip }, function(results, status) {
+     if (status == google.maps.GeocoderStatus.OK) {
+       lat = results[0].geometry.location.lat();
+       lng = results[0].geometry.location.lng();
+
        marker = new google.maps.Marker({
-        position: lift.zip,
+        position: results[0].geometry.location,
         map: map,
         animation: google.maps.Animation.DROP
      });
-   });
+   };
  })
+})
+})
 }
