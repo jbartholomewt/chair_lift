54ac885c5146fe5482889ae39189a05124761968
jbartholomewt <jbartholomewt@gmail.com>
2016-08-17 18:45:29 -0400

    functioning spiderfy feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,8 @@
 //= require skel.min
 //= require util
 //= require main
-//= require_tree .
+// = require_tree .
+// = require oms.min
 
 var map;
 var request;
@@ -23,52 +24,53 @@ var geocoder;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40, lng: -70},
-    zoom: 5
+    center: {lat: 41.850033, lng: -94.6500523},
+    zoom: 4
   });
+  var oms = new OverlappingMarkerSpiderfier(map, {keepSpiderfied: true});
 
- request = $.ajax({
-     method: "GET",
-     url: "/api/lifts"
-   });
-   request.done(function(data) {
-     data.lifts.forEach(function(lift){
-       var min = .999999;
-       var max = 1.000001;
-       var marker;
-       var lat = '';
-       var lng = '';
-       var infowindow;
-       geocoder = new google.maps.Geocoder();
-       geocoder.geocode( { 'address': lift.zip }, function(results, status) {
-         if (status == google.maps.GeocoderStatus.OK) {
-           lat = results[0].geometry.location.lat();
-           lng = results[0].geometry.location.lng();
+  request = $.ajax({
+    method: "GET",
+    url: "/api/lifts"
+  });
+  request.done(function(data) {
+    data.lifts.forEach(function(lift){
+      var marker;
+      var lat = '';
+      var lng = '';
+      var infowindow;
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': lift.zip }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          lat = results[0].geometry.location.lat();
+          lng = results[0].geometry.location.lng();
 
-           marker = new google.maps.Marker({
-             position: results[0].geometry.location,
-             map: map,
-             animation: google.maps.Animation.DROP
-           });
-           var contentString = '<div>'+
-           '<h1>' + lift.name + '</h1>'+
-           '<div>'+
-           '<p> <b>Description: </b>' + lift.description + '</p>'+
-           '<p> <b>Departure: </b>' + lift.departure + '</p>'+
-           '<p> <b>Destination: </b>' + lift.destination + '</p>'+
-           '<p> <b>Departure Date: </b>' + lift.departure_date + '</p>'+
-           '<p> <a href="/lifts/' + lift.id + '"> Click Here For More Info</a> ' + '</p>'+
-           '</div>'+
-           '</div>';
-           infowindow = new google.maps.InfoWindow({
-             content: contentString
-           });
-           marker.addListener('click', function() {
-           infowindow.open(map, marker);
-         });
-         };
-       })
-
-})
-})
+          marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: map,
+            animation: google.maps.Animation.DROP
+          });
+          var contentString = '<div>'+
+          '<h1>' + lift.name + '</h1>'+
+          '<div>'+
+          '<p> <b>Description: </b>' + lift.description + '</p>'+
+          '<p> <b>Departure: </b>' + lift.departure + '</p>'+
+          '<p> <b>Destination: </b>' + lift.destination + '</p>'+
+          '<p> <b>Departure Date: </b>' + lift.departure_date + '</p>'+
+          '<p> <a href="/lifts/' + lift.id + '"> Click Here For More Info</a> ' + '</p>'+
+          '</div>'+
+          '</div>';
+          infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+          oms.addListener('spiderfy', function(marker) {
+          });
+          oms.addMarker(marker);
+        };
+        marker.addListener('click', function(markers) {
+          infowindow.open(map, marker);
+        });
+      })  
+    })
+  })
 }
